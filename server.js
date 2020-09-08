ea83b8f147066822eac372c6d52fc381787c0c86
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-09-08 16:42:08 -0500

    views

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.static("public"));
+
+//routes
+
+app.use(require("./routes/view"));
+
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
 });
-
