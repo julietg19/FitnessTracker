551594b73b31c0f63d6538fdbdab40f935fe8c33
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-09-10 14:13:12 -0500

    apiroutes

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,17 @@
 // display info
 
+const router = require("express").Router();
+
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
